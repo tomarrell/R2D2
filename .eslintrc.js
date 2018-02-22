746cf468ee9a4d5262a643564f699c9b8d462b52
Tom Arrell <tom.arrell@gmail.com>
2018-02-23 01:02:02 +1300

    Fleshed out HOC + tooling

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,8 @@
 module.exports = {
+  parser: 'babel-eslint',
   extends: 'airbnb',
   rules: {
     'react/jsx-filename-extension': 'off',
+    'react/no-did-mount-set-state': 'off',
   },
 };
--- src/index.js
+++ src/index.js
@@ -1,15 +1,80 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-export default function (component, ...passthrough) {
-  return class R2HOC extends Component {
+export default function (WrappedComponent) {
+  class R2HOC extends Component {
     constructor(props) {
       super(props);
 
-      this.state = {};
+      this.state = {
+        valid: false,
+      };
+    }
+
+    componentDidMount() {
+      const {
+        selector,
+        validateState,
+      } = this.props;
+      const { store } = this.context;
+
+      console.log(store);
+      const selectedState = selector(store.getState());
+
+      if (validateState(selectedState)) {
+        this.setState({
+          valid: true,
+        });
+      }
+
+      this.setState({ valid: false });
+    }
+
+    checkState = () => {
+      const { store } = this.context;
+      const { selector, validateState } = this.props;
+
+      return validateState(selector(store.getState()));
     }
 
     render() {
-      return <div {...passthrough} />;
+      const { valid } = this.state;
+      const { altComponent: Alt, ...passThroughProps } = this.props;
+
+      console.log(this.props);
+
+      if (valid) {
+        return (
+          <WrappedComponent
+            {...passThroughProps}
+          />
+        );
+      }
+
+      if (Alt) return <Alt />;
+      return null;
     }
+  }
+
+  R2HOC.contextTypes = {
+    store: PropTypes.object.isRequired,
   };
+
+  R2HOC.propTypes = {
+    selector: PropTypes.func.isRequired,
+    action: PropTypes.shape({
+      type: PropTypes.string,
+      payload: PropTypes.object,
+    }).isRequired,
+
+    validateState: PropTypes.func,
+    altComponent: PropTypes.node,
+  };
+
+  R2HOC.defaultProps = {
+    validateState: state => !!state,
+    altComponent: null,
+  };
+
+  return R2HOC;
 }
