82643ad3545cdbf55cc181d73a37412c347eaad1
Tom Arrell <tom.arrell@gmail.com>
2018-03-01 11:35:41 +1300

    Added support for selector changing dispatching new action

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# R2HOC
-A React HOC for ensuring your data makes it to the component.
+# R2D2
+A React HOC for ensuring your data makes it to your component in time.
 
 ## License
 MIT
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export default function (WrappedComponent) {
-  class R2HOC extends Component {
+  class R2D2 extends Component {
     constructor(props) {
       super(props);
 
@@ -13,33 +13,45 @@ export default function (WrappedComponent) {
     }
 
     componentDidMount() {
-      const { store } = this.context;
-      const { action } = this.props;
-
-      this.validateStore();
       this.updateSubscription();
-      store.dispatch(action);
+      const isValid = this.validateStore();
+
+      if (!isValid) {
+        this.dispatchAction();
+      }
     }
 
+    /*
+     * Replace selector, action from
+     * props should they change.
+     */
     componentDidUpdate(prevProps) {
       const { selector: newSelector } = this.props;
 
+      // If R2D2 receives a new selector, it will validate it again.
       if (newSelector !== prevProps.selector) {
         if (this.validateStore()) {
           this.handleStoreChange();
           return;
         }
 
-        this.updateSubscription();
+        this.dispatchAction();
       }
     }
 
+    /*
+     * Unsubscribe from store.
+     */
     componentWillUnmount() {
       if (this._subscription) {
         this._subscription();
       }
     }
 
+    /*
+     * Get new state using selector
+     * from props.
+     */
     getSelectedState = () => {
       const { store } = this.context;
       const { selector } = this.props;
@@ -47,18 +59,46 @@ export default function (WrappedComponent) {
       return selector(store.getState());
     }
 
+    /*
+     * Dispatch the passed through action
+     * to Redux store.
+     */
+    dispatchAction = () => {
+      const { action } = this.props;
+      const { store } = this.context;
+
+      store.dispatch(action);
+    }
+
+    /*
+     * Handler to run when Redux store
+     * is updated. Grab new selected state.
+     */
     handleStoreChange = () => {
       this.setState({
+        valid: this.validateStore(),
         data: this.getSelectedState(),
       });
     }
 
+    /*
+     * Setup or replace the store subscription
+     * handler.
+     */
     updateSubscription = () => {
       const { store } = this.context;
+
+      if (this._subscription) {
+        this._subscription();
+      }
 
       this._subscription = store.subscribe(this.handleStoreChange);
     }
 
+    /*
+     * Check if the selected part of store
+     * passes the validation function.
+     */
     validateStore = () => {
       const { validateStore } = this.props;
 
@@ -75,8 +115,6 @@ export default function (WrappedComponent) {
       const { valid, data } = this.state;
       const { altComponent: Alt, ...passThroughProps } = this.props;
 
-      console.log(data);
-
       if (!Alt || valid) {
         return (
           <WrappedComponent
@@ -91,11 +129,11 @@ export default function (WrappedComponent) {
     }
   }
 
-  R2HOC.contextTypes = {
+  R2D2.contextTypes = {
     store: PropTypes.object.isRequired,
   };
 
-  R2HOC.propTypes = {
+  R2D2.propTypes = {
     selector: PropTypes.func.isRequired,
     action: PropTypes.shape({
       type: PropTypes.string,
@@ -109,10 +147,10 @@ export default function (WrappedComponent) {
     ]),
   };
 
-  R2HOC.defaultProps = {
+  R2D2.defaultProps = {
     validateStore: state => !!state,
     altComponent: null,
   };
 
-  return R2HOC;
+  return R2D2;
 }
